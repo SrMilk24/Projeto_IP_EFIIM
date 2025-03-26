d5c554ed922a52e4695f10f6dbee7cc069bd9e30
Felipe Milk <fsl8@cin.ufpe.br>
2025-03-26 18:47:58 -0300

    dhusahsau

--- assets/images/duda.py
+++ assets/images/duda.py
@@ -1,4 +1,20 @@
-print("chablau")
+class Calopsita:
+    def __init__(self, nome, aveia, velocidade, golpe):
+        self.nome = nome
+        self.aveia = aveia
+        self.velocidade = velocidade
+        self.golpe =golpe
 
-if True:
-    print("Fuck Python")
+        return None
+    
+    def __str__(self):
+        return "member of Test"
+    # Todas as funções no use_case
+    def set_nome(self, nome):
+        self.nome = nome
+
+juliano = Calopsita("Juliano", 10, 10, ["bicada atomica super poderosa"])
+marcelo = Calopsita("Marcelo", 10, 10, ["Bundada de tanajura"])
+
+print(juliano.golpe)
+print(marcelo.golpe)
